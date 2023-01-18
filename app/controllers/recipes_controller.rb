d3d1b1878381fce979aafd61afef866b0d26cadf
Jonny <jonny.howle@gmail.com>
2023-01-18 16:16:45 -0700

    commit tree 91f66133179d6ff966b536a75748ed6fb7b49ebc

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,21 @@
+class RecipesController < ApplicationController
+
+
+    def index
+        recipes = Recipe.all 
+        render json: recipes, include: :user
+      end
+    
+      def create
+        user = User.find_by(id: session[:user_id])
+        recipe = user.recipes.create!(recipe_params)
+        render json: recipe, include: :user, status: :created
+      end
+    
+      private
+    
+      def recipe_params
+        params.permit(:title, :instructions, :minutes_to_complete)
+      end
+    
+    end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post '/signup', to: "users#create"
+  get "/me", to: "users#show"
+  delete "/logout", to: "sessions#destroy"
+  post '/login', to: "sessions#create"
+  post '/recipes', to: "recipes#create"
+  get '/recipes', to: "recipes#index"
 end
